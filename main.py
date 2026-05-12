95e6c6a8442028297f0abef3d2d6827517e2cdfb
CodingPandarian <zavien.d.leijen@gmail.com>
2026-05-12 18:48:37 +0200

    CH2:L4

--- main.py
+++ main.py
@@ -7,18 +7,20 @@ from logger import log_event, log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill("black")
         pygame.display.flip()
 
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
-
 
 if __name__ == "__main__":
     main()
